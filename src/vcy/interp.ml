3e35017c786c0982e214ff3db2c059fb2f4a1a38
Crazycolorz5 <Crazycolorz5@gmail.com>
2024-05-17 17:35:20 -0400

    Preliminary implementation of new_job.

--- src/vcy/interp.ml
+++ src/vcy/interp.ml
@@ -25,6 +25,21 @@ let pool_size = 4
 let debug_print (s : string lazy_t) =
   if !debug_display then print_string (Lazy.force s); flush stdout
 
+(* A Job is an unit of work, consisting of:
+   - task ID that should perform the job
+   - input data for that job is provided in the
+     environment as a local, at the topmost call stack frame
+  - all non-input will share access (Via references) to the
+    shared environment
+*)
+type job = {
+  tid: int;
+  env: env;
+  (* the environment will have in the stack the input variables:
+  vals: (ty * id * value) list 
+  exp will only be constants: CInt, CStr, CBool, etc
+  *)
+}
 
 (*** ENVIRONMENT MANAGEMENT ***)
 
@@ -676,7 +691,7 @@ and interp_stmt (env : env) (stmt : stmt node) : env * value option =
           end          
        ) [] var_id_list in
        let env' = senddep_extend_env env job_vals in
-       Parallel.new_job task_id env';
+       (* Parallel. TODO: make modular? *) new_job task_id env';
        (* now just return the unmodified environment *)
        env, None
  
@@ -747,7 +762,63 @@ and interp_block (env : env) (block : block node) : env * value option =
     else env
   in env, ret
 
+(* let schedule_task tsk () *)
 
+
+(* PS-DSWP Execution Mode *)
+
+
+and job_queue = Queue.create ()
+and run_job jb : unit = 
+    let (env',v) = interp_block jb.env (load_task_def jb.tid).body in
+    ()
+(* Interpreter calls this function at each SendDep to create a new job *)
+and new_job t e = debug_print (Lazy.from_val (sp "Starting new job with tid=%d.\n" t)); Domainslib.Task.run !pool (fun () -> run_job {tid=t; env=e}) (* Queue.add {tid=t; env=e} job_queue *)
+
+and task_defs = ref []
+and pool = ref (Domainslib.Task.setup_pool ~num_domains:pool_size ())
+and set_task_def tlist = task_defs := tlist
+and load_task_def (taskid:int) : dswp_task = 
+  try List.find (fun t -> t.id == taskid) !task_defs
+  with Not_found -> failwith "could not find task id"
+
+and scheduler poolsize task_list : unit =
+  (* Create a domain pool with four worker domains *)
+  (* create a function to quickly return a task by id *)
+  (* let tid2task = List.fold_left (fun acc tsk -> 
+        (fun tid -> if tsk.id == tid then tsk else (acc tid))
+    ) (fun _ -> failwith "could not find task id") task_list in *)
+
+     (* env = local callstack and globals *)
+     (* 1. how do we make sure that mutation to shared vars? *)
+     (* 2. how do we extend env to include the input vals? *)
+       
+    (* Idea:
+        all variables are by default global.
+        at the beginning of a task, input dependences are local.
+        interp a variable,
+       *)
+
+        (* what about accuulated senddeps that need to become new jobs? *)
+
+
+  Domainslib.Task.run !pool (fun () ->
+    let rec loop promises =
+      match Queue.take_opt job_queue with
+      | Some j ->
+          begin 
+            let pr' = Domainslib.Task.async !pool (fun () ->
+              (* use doall information to have replicas of a task? *)
+              Printf.printf "about to exec a new job for task %d\n" j.tid;
+              run_job j) in
+            loop (pr'::promises)
+          end
+      | None -> 
+          Printf.printf "scheduler: reached an empty queue. need to now wait to join!\n";
+          promises
+    in
+    let promises' = loop [] in
+    List.map (Domainslib.Task.await !pool) promises') |> ignore
 
 
 (*** COMMUTATIVITY INFERENCE ***)
@@ -1287,82 +1358,6 @@ let prepare_prog (prog : prog) (argv : string array) =
   let e = CallRaw (main_method_name, [e_argc;e_argv]) |> no_loc in
   env, e
 
-(* PS-DSWP Execution Mode *)
-
-(* A Job is an unit of work, consisting of:
-   - task ID that should perform the job
-   - input data for that job is provided in the
-     environment as a local, at the topmost call stack frame
-  - all non-input will share access (Via references) to the
-    shared environment
-*)
-type job = {
-  tid: int;
-  env: env;
-  (* the environment will have in the stack the input variables:
-  vals: (ty * id * value) list 
-  exp will only be constants: CInt, CStr, CBool, etc
-  *)
-}
-
-(* let schedule_task tsk () *)
-
-let task_defs = ref []
-let set_task_def tlist = task_defs := tlist
-let load_task_def (taskid:int) : dswp_task = 
-  try List.find (fun t -> t.id == taskid) !task_defs
-  with Not_found -> failwith "could not find task id"
-
-let job_queue = Queue.create ()
-
-(* Interpreter calls this function at each SendDep to create a new job *)
-let new_job t e = Queue.add {tid=t; env=e} job_queue
-
-let scheduler poolsize task_list : unit =
-  (* Create a domain pool with four worker domains *)
-  let pool = Domainslib.Task.setup_pool ~num_domains:poolsize () in
-
-  (* create a function to quickly return a task by id *)
-  (* let tid2task = List.fold_left (fun acc tsk -> 
-        (fun tid -> if tsk.id == tid then tsk else (acc tid))
-    ) (fun _ -> failwith "could not find task id") task_list in *)
-
-  let run_job jb : unit = 
-    let (env',v) = interp_block jb.env (load_task_def jb.tid).body in
-    ()
-  in
-
-     (* env = local callstack and globals *)
-     (* 1. how do we make sure that mutation to shared vars? *)
-     (* 2. how do we extend env to include the input vals? *)
-       
-    (* Idea:
-        all variables are by default global.
-        at the beginning of a task, input dependences are local.
-        interp a variable,
-       *)
-
-        (* what about accuulated senddeps that need to become new jobs? *)
-
-
-  Domainslib.Task.run pool (fun () ->
-    let rec loop promises =
-      match Queue.take_opt job_queue with
-      | Some j ->
-          begin 
-            let pr' = Domainslib.Task.async pool (fun () ->
-              (* use doall information to have replicas of a task? *)
-              Printf.printf "about to exec a new job for task %d\n" j.tid;
-              run_job j) in
-            loop (pr'::promises)
-          end
-      | None -> 
-          Printf.printf "scheduler: reached an empty queue. need to now wait to join!\n";
-          promises
-    in
-    let promises' = loop [] in
-    List.map (Domainslib.Task.await pool) promises') |> ignore;
-  Domainslib.Task.teardown_pool pool (* Clean up the pool we allocated *)
 
 let interp_tasks env0 decls tasks : unit =
   set_task_def tasks;
